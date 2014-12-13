b5ad13153450c868a1a20fb3985523f134a12529
Erty Seidel <ertysdl+github@gmail.com>
2014-12-13 18:58:42 -0500

    Remove old player-spaceship code

--- js/classes/GamePlayer.js
+++ js/classes/GamePlayer.js
@@ -30,32 +30,11 @@
 			"right": false
 		};
 
-		this.solarSystemCollisions = [];
-
 		this.draw = function(ctx){
 			if(!this.visible) return;
-			//draw player
-			switch(this.style){
-				case "space ship":
-					this.spaceshipDraw(ctx);
-					break;
-				case "human":
-					this.humanDraw(ctx);
-					break;
-			}
-		};
-
-		this.humanDraw = function(ctx){
 			ctx.strokeStyle = _.settings.color_player_human;
 			ctx.beginPath();
 			ctx.arc(this.pos.x + this.size.x / 2, this.pos.y + this.size.y / 2, this.size.x / 2, 0, 2*Math.PI);
-			ctx.stroke();
-		};
-
-		this.spaceshipDraw = function(ctx){
-			ctx.strokeStyle = _.settings.color_player_spaceship;
-			ctx.beginPath();
-			ctx.arc(this.pos.x + this.size.x / 2, this.pos.y + this.size.y / 2, 5, 0, 2*Math.PI);
 			ctx.stroke();
 		};
 
@@ -102,65 +81,32 @@
 		};
 
 		this.collision = function(other){
-			({
-				"human": this.humanCollision,
-				"space ship": this.spaceshipCollision
-			})[this.style].bind(this)(other);
-		};
-
-		this.spaceshipCollision = function(other){
-			if(other instanceof GameSolarSystem && this.solarSystemCollisions.indexOf(other) == -1){
-				this.solarSystemCollisions.push(other);
-			}
-			if(this.solarSystemCollisions.length > 0){
-				this.solarSystemCollisions.sort(function(a, b){
-					return stationDist(this, a) - stationDist(this, b);
-				}.bind(this));
-				this.solarSystemCollisions[0].collided = true;
-				for(var i = 1; i < this.solarSystemCollisions.length; i++){
-					this.solarSystemCollisions[i].collided = false;
-				}
-			}
-		};
-
-		this.humanCollision = function(other){
 			if(other instanceof GameCollisionBox){
 				other.collided = true;
 				//UP
 				if(this.pos.y <= other.pos.y + other.size.y &&
 					this.pos.y + this.size.y / 2 > other.pos.y){
-					//this.pos.y = other.pos.y + other.size.y;
 					this.wallCollisions.up = other;
 				}
 				//DOWN
 				if(this.pos.y + this.size.y >= other.pos.y &&
 					this.pos.y + this.size.y / 2 < other.pos.y){
-					//this.pos.y = other.pos.y - this.size.y;
 					this.wallCollisions.down = other;
 				}
 				//LEFT
 				if(this.pos.x <= other.pos.x + other.size.x &&
 					this.pos.x + this.size.x / 2 > other.pos.x + other.size.x){
-					//this.pos.x = other.pos.x + other.size.x;
 					this.wallCollisions.left = other;
 				}
 				//RIGHT
 				if(this.pos.x + this.size.x >= other.pos.x &&
 					this.pos.x + this.size.x / 2 < other.pos.x){
-					//this.pos.x = other.pos.x - this.size.x;
 					this.wallCollisions.right = other;
 				}
 			}
 		};
 
 		this.uncollision = function(other){
-			({
-				"human": this.humanUncollision,
-				"space ship": this.spaceshipUncollision
-			})[this.style].bind(this)(other);
-		};
-
-		this.humanUncollision = function(other){
 			if(other instanceof GameCollisionBox){
 				other.collided = false;
 				if(other == this.wallCollisions.up) this.wallCollisions.up = false;
@@ -169,20 +115,6 @@
 				if(other == this.wallCollisions.right) this.wallCollisions.right = false;
 			}
 		};
-
-		this.spaceshipUncollision = function(other){
-			other.collided = false;
-			if(other instanceof GameSolarSystem){
-				this.solarSystemCollisions.splice(this.solarSystemCollisions.indexOf(other), 1);
-			}
-		};
-	};
-
-	function stationDist(player, station){
-		return Math.sqrt(
-			Math.pow(player.pos.x - (station.pos.x + (station.size.x / 2)), 2)
-			+ Math.pow(player.pos.y - (station.pos.y + (station.size.y / 2)), 2)
-		);
 	}
 
 	exports.GamePlayer = GamePlayer;
